ade3561adc0ab490ae5e750fa799c78838d38477
winpenning <zs.phenriqueprofessional@gmail.com>
2023-04-27 14:16:39 -0300

    Projeto inicialmente finalizado

--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
+using System.Globalization;
 using Interfaces.entities;
+using Interfaces.services;
+
 namespace Interfaces
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var ptBR = new System.Globalization.CultureInfo("pt-BR");
             Console.WriteLine("Enter rental data");
             Console.Write("Car model: ");
             string carmodel = Console.ReadLine();
             Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
-            string pickupdate = Console.ReadLine();
+            DateTime start = DateTime.Parse(Console.ReadLine(), ptBR);
             Console.Write("Return (dd/mm/yyyy hh:mm): "); 
-            string returndate = Console.ReadLine();
+            DateTime finish = DateTime.Parse(Console.ReadLine(), ptBR);
             Console.Write("Enter price per hour: ");
             double priceperhour = double.Parse(Console.ReadLine());
             Console.Write("Enter the price per day: ");
             double priceperday = double.Parse(Console.ReadLine());
 
-
+            CarRental carRental = new CarRental(start, finish, new Vehicle(carmodel));
+            RentalService rentalService = new RentalService(priceperhour, priceperday, new BrazilTaxService());
+            rentalService.ProcessInvoice(carRental);
             Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.getInvoice());
         }
     }
 }
